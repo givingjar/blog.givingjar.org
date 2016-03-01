8806b0db7f3e4382975226c1a4231b5a93299e76
Erik Gillespie <erik.gillespie@gmail.com>
2016-03-01 13:37:10 -0500

    Grunt tasks to deploy latest to prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,23 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
+    gitfetch: {
+      latest: {
+        options: {
+          all: true
+        }
+      }
+    },
+    
+    gitreset: {
+      latest: {
+        options: {
+          mode: 'hard',
+          commit: 'origin/master'
+        }
+      }
+    },
+  
     jekyll: {
       dist: {
         options: {
@@ -33,14 +50,21 @@ module.exports = function(grunt) {
           project: '_site.qa/',
           domain: 'givingjarblog.surge.sh'
         }
+      },
+      prod: {
+        options: {
+          project: '_site/'
+        }
       }
     }
   });
   
+  grunt.loadNpmTasks('grunt-git');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-surge');
   
   grunt.registerTask('default', ['jekyll:dist']);
   grunt.registerTask('qa', ['jekyll:qa', 'surge:qa']);
+  grunt.registerTask('prod', ['gitfetch:latest', 'gitreset:latest', 'jekyll:dist']);
   grunt.registerTask('test', ['jekyll:test']);
 };
